14554c49ed866318a451ce5fd970b33735b02f1e
NithyaNN3 <nithyaniranjani@gmail.com>
2025-04-29 23:20:06 +1000

    test change

--- test/components/envelope_test.dart
+++ test/components/envelope_test.dart
@@ -10,60 +10,67 @@ void main() {
       Position.named(lat: 102.0, lng: 0.5),
       Position.named(lat: 103.0, lng: 1.5),
       Position.named(lat: 104.0, lng: 2.5),
-    ])
+      ],
+    ),
   );
 
   final poly = Feature<Polygon>(
       geometry: Polygon(coordinates: [
-    [
-      Position.named(lat: 101.0, lng: 0.0),
-      Position.named(lat: 101.0, lng: 1.0),
-      Position.named(lat: 100.0, lng: 1.0),
-      Position.named(lat: 100.0, lng: 0.0),
-      Position.named(lat: 101.0, lng: 0.0),
-    ],
-  ]));
+        [
+          Position.named(lat: 101.0, lng: 0.0),
+          Position.named(lat: 101.0, lng: 1.0),
+          Position.named(lat: 100.0, lng: 1.0),
+          Position.named(lat: 100.0, lng: 0.0),
+          Position.named(lat: 101.0, lng: 0.0),
+        ],
+      ],
+    ),
+  );
 
   final multiLine = Feature<MultiLineString>(
       geometry: MultiLineString(coordinates: [
-    [
-      Position.named(lat: 100.0, lng: 0.0),
-      Position.named(lat: 101.0, lng: 1.0),
-    ],
-    [
-      Position.named(lat: 102.0, lng: 2.0),
-      Position.named(lat: 103.0, lng: 3.0),
-    ],
-  ]));
+        [
+          Position.named(lat: 100.0, lng: 0.0),
+          Position.named(lat: 101.0, lng: 1.0),
+        ],
+        [
+          Position.named(lat: 102.0, lng: 2.0),
+          Position.named(lat: 103.0, lng: 3.0),
+        ],
+      ],
+    ),
+  );
 
   final multiPoly = Feature<MultiPolygon>(
       geometry: MultiPolygon(coordinates: [
-    [
-      [
-        Position.named(lat: 102.0, lng: 2.0),
-        Position.named(lat: 103.0, lng: 2.0),
-        Position.named(lat: 103.0, lng: 3.0),
-        Position.named(lat: 102.0, lng: 3.0),
-        Position.named(lat: 102.0, lng: 2.0),
+        [
+          [
+            Position.named(lat: 102.0, lng: 2.0),
+            Position.named(lat: 103.0, lng: 2.0),
+            Position.named(lat: 103.0, lng: 3.0),
+            Position.named(lat: 102.0, lng: 3.0),
+            Position.named(lat: 102.0, lng: 2.0),
+          ],
+        ],
+        [
+          [
+            Position.named(lat: 100.0, lng: 0.0),
+            Position.named(lat: 101.0, lng: 0.0),
+            Position.named(lat: 101.0, lng: 1.0),
+            Position.named(lat: 100.0, lng: 1.0),
+            Position.named(lat: 100.0, lng: 0.0),
+          ],
+          [
+            Position.named(lat: 100.2, lng: 0.2),
+            Position.named(lat: 100.8, lng: 0.2),
+            Position.named(lat: 100.8, lng: 0.8),
+            Position.named(lat: 100.2, lng: 0.8),
+            Position.named(lat: 100.2, lng: 0.2),
+          ],
+        ],
       ],
-    ],
-    [
-      [
-        Position.named(lat: 100.0, lng: 0.0),
-        Position.named(lat: 101.0, lng: 0.0),
-        Position.named(lat: 101.0, lng: 1.0),
-        Position.named(lat: 100.0, lng: 1.0),
-        Position.named(lat: 100.0, lng: 0.0),
-      ],
-      [
-        Position.named(lat: 100.2, lng: 0.2),
-        Position.named(lat: 100.8, lng: 0.2),
-        Position.named(lat: 100.8, lng: 0.8),
-        Position.named(lat: 100.2, lng: 0.8),
-        Position.named(lat: 100.2, lng: 0.2),
-      ],
-    ],
-  ]));
+    ),
+  );
 
   final fc =
       FeatureCollection(features: [point, line, poly, multiLine, multiPoly]);
