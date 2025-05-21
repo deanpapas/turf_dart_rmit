86faeb72ff8effdf18bc9b76d19ded83333246b7
NithyaNN3 <nithyaniranjani@gmail.com>
2025-05-21 23:54:59 +1000

    basic bboxclip implementation

--- lib/src/bboxclip.dart
+++ lib/src/bboxclip.dart
@@ -1,12 +1,82 @@
-// This code considers the dart port of lineclip from Mapbox
-// https://github.com/Zverik/flutter_country_coder/blob/eccf7afbc13746d864b01fd581c7b785499340f0/lib/src/lineclip.dart#L8
+import 'package:geotypes/geotypes.dart';
+import 'package:turf/helpers.dart';
+import 'package:turf/bbox.dart'; // for BBox
+import 'package:turf/lineclip.dart'; // your polygonclip and lineclip implementations
 
-import 'package:geotypes/geotypes.dart' show
-  Feature,
-  LineString,
-  MultiLineString,
-  MultiPolygon,
-  Polygon;
+Feature bboxClip(Feature feature, BBox bbox) {
+  final geometry = feature.geometry;
 
-import 'package:turf/bbox.dart'; 
-import 'package:turf/lineclip.dart';
+  switch (geometry) {
+    case LineString():
+      final clippedLines = lineclip(geometry.coordinates, bbox);
+      if (clippedLines.length == 1) {
+        return Feature(
+          geometry: LineString(clippedLines.first),
+          properties: feature.properties,
+        );
+      }
+      return Feature(
+        geometry: MultiLineString(clippedLines),
+        properties: feature.properties,
+      );
+
+    case MultiLineString():
+      final List<List<Position>> resultLines = [];
+      for (final line in geometry.coordinates) {
+        lineclip(line, bbox, resultLines);
+      }
+      if (resultLines.length == 1) {
+        return Feature(
+          geometry: LineString(resultLines.first),
+          properties: feature.properties,
+        );
+      }
+      return Feature(
+        geometry: MultiLineString(resultLines),
+        properties: feature.properties,
+      );
+
+    case Polygon():
+      final clippedRings = clipPolygon(geometry.coordinates, bbox);
+      return Feature(
+        geometry: Polygon(clippedRings),
+        properties: feature.properties,
+      );
+
+    case MultiPolygon():
+      final clippedPolygons = geometry.coordinates
+          .map((rings) => clipPolygon(rings, bbox))
+          .where((poly) => poly.isNotEmpty)
+          .toList();
+
+      return Feature(
+        geometry: MultiPolygon(clippedPolygons),
+        properties: feature.properties,
+      );
+
+    default:
+      throw UnsupportedError('Geometry type ${geometry.runtimeType} not supported');
+  }
+}
+
+List<List<Position>> clipPolygon(List<List<Position>> rings, BBox bbox) {
+  final List<List<Position>> outRings = [];
+
+  for (final ring in rings) {
+    final clipped = polygonclip(ring, bbox);
+
+    if (clipped.isNotEmpty) {
+      // Ensure the ring is closed
+      if (clipped.first != clipped.last) {
+        clipped.add(clipped.first);
+      }
+
+      // Minimum 4 points to form a valid ring
+      if (clipped.length >= 4) {
+        outRings.add(clipped);
+      }
+    }
+  }
+
+  return outRings;
+}
