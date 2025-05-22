77c2fa06f726667a0d699327c6f6d57d7adf7c9c
tiinsy <s3900481@student.rmit.edu.au>
2025-05-23 00:57:10 +1000

    final implentation randomPosition - includes testing

---
+++ test/components/random_position_test.dart
@@ -0,0 +1,31 @@
+import 'package:turf/random_position.dart';
+import 'package:test/test.dart';
+
+void main() { 
+  group('Random Position tests', () {
+    test('Generating a random position within a bounding box', () {
+      BBox bbox = BBox(100.0, -24.0, 110.0, -23.0);
+      Position randomPos = randomPosition(bbox);
+
+      expect(randomPos.lng, greaterThanOrEqualTo(bbox[0]!));
+      expect(randomPos.lng, lessThanOrEqualTo(bbox[2]!));
+      expect(randomPos.lat, greaterThanOrEqualTo(bbox[1]!));
+      expect(randomPos.lat, lessThanOrEqualTo(bbox[3]!));
+    });
+
+    test('Generating a random position when no bbox is provided', () {
+      Position randomPos = randomPosition(null);
+      expect(randomPos.lng, inInclusiveRange(-180, 180));
+      expect(randomPos.lat, inInclusiveRange(-90, 90));
+    });
+
+    test('Check coordInBox returns point within bbox', () {
+      BBox bbox = BBox(100.0, -24.0, 110.0, -23.0);
+      Position coord = coordInBBox(bbox);
+      expect(coord.lng, greaterThanOrEqualTo(bbox[0]!));
+      expect(coord.lng, lessThanOrEqualTo(bbox[2]!));
+      expect(coord.lat, greaterThanOrEqualTo(bbox[1]!));
+      expect(coord.lat, lessThanOrEqualTo(bbox[3]!));
+    });
+  });
+}
